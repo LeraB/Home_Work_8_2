ced8d5adbc025b0bea7b77bf71e3ebfb9823fcb2
LeraB <kudravka13@gmail.com>
2017-12-22 09:37:33 +0200

    contact us block

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { HashRouter} from 'react-router-dom'
+
+import './App.css';
+import './style/Services.css'
+import './style/Home.css'
+import './css/main.css'
+
+// import scrollToComponent from 'react-scroll-to-component'
+// import Scroll from 'react-scroll'
+// import {scroller} from 'react-scroll'
+
+import Home from './components/Home'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
+import OurTeam from './components/OurTeam'
+import Blog from './components/Blog'
+import ContactUs from './components/ContactUs'
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <Home />
+          <Services />
+          <Portfolio />
+          <OurTeam />
+          <Blog />
+          <ContactUs />
+          <p className='footer'> <i> &#169; Copyright 2013 By Projex - All rights Reserved</i> </p>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App
---
+++ src/components/ContactUs.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class ContactUs extends  Component {
+  render () {
+    return (
+      <div className='contact-us'>
+              <div className="wraper">
+        <h3>The Latest News</h3>
+        <div className ='line-container'>
+        <hr className='left'/>
+        <span className='dot'></span>
+        <hr className='right'/>
+        </div>
+            <div>
+        <div className="contuct-form">
+        <input type="text" name="browser" placeholder="Full Name"/>
+        <input type="text" name="browser" placeholder="E-mail Adress"/>
+        <input type="text" name="browser" placeholder="Message"  className="textarea"/>
+        </div>
+            <div class="information-block">
+            <h4>Information</h4>
+            <p>
+        Proin nec dignissim tellus, ut hendrerit erat. Vivamus ut augue ac est sagittis ullamcorper a porttitor nibh. Donec non tortor consequat, sollicitudin odio id, vehicula lorem. Suspendisse a euismod sem. Mauris rutrum arcu a lorem aliquet feugiat. Praesent tincidunt lacus at nisl pharetra, nec suscipit risus lobortis. Nunc vitae enim a quam ultrices faucibus. Proin sit amet condimentum libero, ac dignissim enim. Praesent aliquam lacinia dapibus.
+        </p>
+        <span>StreetName 163, 254 23 City</span>
+        <span className="phone">(+254) 001 454 333 66</span>
+
+       <a href="#" className="send">SEND MESSAGE</a>
+        <a href="#" className="view">VIEW MAP</a>
+        </div>
+        </div>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default ContactUs
